4eb529c56393c4fd986292e0200a817f2744ad2c
Mr.Fazli <yasinfazli620@gmail.com>
2023-11-11 03:15:27 -0800

    valicarion login

--- src/app/ui/login/component/login/login.component.ts
+++ src/app/ui/login/component/login/login.component.ts
@@ -15,7 +15,7 @@ export class LoginComponent implements OnInit {
 
   constructor(private router: Router, private loginService: LoginService, private _snackBar: MatSnackBar) {
     this.loginForm = new FormGroup({
-      email: new FormControl(null, [Validators.required]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, [Validators.required]),
     })
   }
